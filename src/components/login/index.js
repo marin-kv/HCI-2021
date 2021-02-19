29b177ceafc70d988aa4040b4c9466337a748846
Petar <plabet00@fesb.hr>
2021-02-19 19:18:05 +0100

    Add registration functionality

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -4,7 +4,7 @@ import Login_icon from "../Images/login_icon"
 import { Modal } from 'react-responsive-modal';
 import 'react-responsive-modal/styles.css';
 
-function checkCredentials(username, password, setOpenLogin, setFailedLogin) {
+function checkCredentialsLogin(username, password, setOpenLogin, setFailedLogin) {
     const users = [{ username: "Marin", password: "1234" }, { username: "Petar", password: "12345" }];
     for (let user of users) {
         if (user.username === username && user.password === password) {
@@ -18,11 +18,24 @@ function checkCredentials(username, password, setOpenLogin, setFailedLogin) {
 
 }
 
+function checkCredentialsRegister(username, password, passwordCheck, setOpenRegister, setFailedRegister) {
+    if(passwordCheck === password){
+        localStorage.setItem("username",username);
+        setOpenRegister(false);
+        return true
+    }
+    setFailedRegister(true);
+    return false
+
+}
+
 const Login = () => {
     const [failedLogin, setFailedLogin] = useState(false);
+    const [failedRegister, setFailedRegister] = useState(false);
 
     const [usernameInput, setUsername] = useState("");
     const [passwordInput, setPassword] = useState("");
+    const [passwordCheck, setPasswordCheck] = useState("");
 
     const [openLogin, setOpenLogin] = useState(false);
     const [openRegister, setOpenRegister] = useState(false);
@@ -61,7 +74,7 @@ const Login = () => {
                         <button onClick={() => alert("Žao mi je 😂")} className={styles.forgot}>Zaboravili ste lozinku?</button>
                     </div>
                     <div className={failedLogin === true ? styles.failed_login_true : styles.failed_login_false}>Kriva lozinka i/ili korisničko ime</div>
-                    <button onClick={() => localStorage.setItem("loggedIn", checkCredentials(usernameInput, passwordInput, setOpenLogin, setFailedLogin))} className={styles.login_button}>Prijava</button>
+                    <button onClick={() => localStorage.setItem("loggedIn", checkCredentialsLogin(usernameInput, passwordInput, setOpenLogin, setFailedLogin))} className={styles.login_button}>Prijava</button>
                 </div>
             </Modal>
             <Modal open={openRegister} onClose={onCloseModalRegister} center
@@ -75,15 +88,16 @@ const Login = () => {
                         <input className={styles.input_window} type="text" placeholder="Email" size="50" />
                     </div>
                     <div className={styles.user_pass}>
-                        <input className={styles.input_window} type="text" placeholder="Korisničko ime" size="50" />
+                        <input onChange={(e) => setUsername(e.target.value)} className={styles.input_window} type="text" placeholder="Korisničko ime" size="50" />
                     </div>
                     <div className={styles.user_pass}>
-                        <input className={styles.input_window} type="password" placeholder="Lozinka" size="50" />
+                        <input onChange={(e) => setPassword(e.target.value)} className={styles.input_window} type="password" placeholder="Lozinka" size="50" />
                     </div>
                     <div className={styles.user_pass}>
-                        <input className={styles.input_window} type="password" placeholder="Ponovite lozinku" size="50" />
+                        <input onChange={(e) => setPasswordCheck(e.target.value)} className={styles.input_window} type="password" placeholder="Ponovite lozinku" size="50" />
                     </div>
-                    <button className={styles.login_button}>Registracija</button>
+                    <div className={failedRegister === true ? styles.failed_login_true : styles.failed_login_false}>Pogrešno ponovljena lozinka</div>
+                    <button onClick={() => localStorage.setItem("loggedIn", checkCredentialsRegister(usernameInput, passwordInput, passwordCheck, setOpenRegister, setFailedRegister))} className={styles.login_button}>Registracija</button>
                 </div>
             </Modal>
         </nav>
