bc7ea5c15e1d417021e18c9000957c98e486d99a
marin-kv <marinkovacevicvranjican@gmail.com>
2021-03-03 15:31:27 +0100

    Add additional product page content

--- src/components/ProductSection/index.js
+++ src/components/ProductSection/index.js
@@ -9,13 +9,14 @@ const ProductSection = (props) => {
                 <Img className={styles.productImage} fluid={props.data.allContentfulProduct.edges[0].node.productImage.fluid} />
                 <div className={styles.productUpperText}>
                     <div style={{fontSize: 'xx-large', fontWeight: 'bold'}}>{props.data.allContentfulProduct.edges[0].node.productName}</div>
-                    <div style={{fontSize: 'xx-large'}}>
+                    <div style={{fontSize: 'xx-large', display: 'flex', alignItems: 'center'}}>
                         <span style={{textDecoration: props.data.allContentfulProduct.edges[0].node.productTags.includes('Akcija')? 'line-through' : 'none'}}>
                             {props.data.allContentfulProduct.edges[0].node.productPrice + ',99 kn'}
                         </span>
                         <span style={{ color: 'red', paddingLeft: '20px', display: props.data.allContentfulProduct.edges[0].node.productTags.includes('Akcija')? 'inline' : 'none'}}>
                             {parseInt((props.data.allContentfulProduct.edges[0].node.productPrice + 1) * 0.8) + ',99 kn'}
                         </span>
+                        <input type="number" className={styles.numberInput} min='1' placeholder='1'></input>
                     </div>
                     <button className={styles.cartButton}>Dodaj u košaricu</button>
                 </div>
